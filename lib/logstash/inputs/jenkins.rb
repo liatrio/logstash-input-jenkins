ba23745e945e51104f13767aff15a37ce7017d6e
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-02-14 14:39:19 -0800

    Plugin working and sending logs to plugin

--- lib/logstash/inputs/jenkins.rb
+++ lib/logstash/inputs/jenkins.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require "socket" # for Socket.gethostname
+require "logstash/inputs/http"
 
 # Generate a repeating message.
 #
@@ -15,6 +16,12 @@ class LogStash::Inputs::Jenkins < LogStash::Inputs::Http
   config :host, :validate => :string, :default => "jenkins.liatr.io"
 
   config :port, :validate => :number, :default => 80
+
+  # Username for basic authorization
+  config :user, :validate => :string, :required => false
+
+  # Password for basic authorization
+  config :password, :validate => :password, :required => false
 
   public
 
