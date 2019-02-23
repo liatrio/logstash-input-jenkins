cc83e3f513a66e5eb432863f3dd2b857c832f939
Takumi Nishida <tnishida1@mail.csuchico.edu>
2019-02-22 16:59:43 -0800

    Made plugin more modular and functionized things

--- lib/logstash/inputs/jenkins.rb
+++ lib/logstash/inputs/jenkins.rb
@@ -7,8 +7,6 @@ require "elasticsearch"
 require "pp"
 require "date"
 
-
-
 # This plugin utilizes Jenkins' Logstash plugin linked here:
 #   https://wiki.jenkins.io/display/JENKINS/Logstash+Plugin
 # The plugin will interpret the Jenkins logs sent to logstash and interpret them according to
@@ -18,200 +16,203 @@ require "date"
 # information to elasticsearch.
 
 class LogStash::Inputs::Jenkins < LogStash::Inputs::Http
-
+  
   config_name "jenkins"
-
+  
   # If undefined, Logstash will complain, even if codec is unused.
   default :codec, "json"
-
+  
   config :host, :validate => :string, :default => "jenkins.liatr.io"
-
+  
   config :port, :validate => :number, :default => 80
-
+  
   # Username for basic authorization
   config :user, :validate => :string, :required => false
-
+  
   # Password for basic authorization
   config :password, :validate => :password, :required => false
-
+  
   config :elastic_port, :validate => :number, :default => 9200
-
+  
   config :elastic_host, :validate => :string, :default => '192.168.0.27'
-
+  
   config :elastic_scheme, :validate => :string, :default => 'http'
-
+  
   public
-
+  
+  
+  def get_build_response(client, body_obj)
+    commit_id = body_obj['data']['buildVariables']['GIT_COMMIT']
+    
+    response = client.search index: 'lead_time', body:
+      {
+        "query": {
+          "nested": {
+            "path": "commits",
+            "query": {
+              "term": {
+                "commits.id": {
+                  "value": commit_id
+                }
+              }
+            }
+          }
+        }
+      }
+    return response
+  end
+  
+  def get_deploy_response(client, body_obj)
+    
+    artifact_id = body_obj['data']['buildVariables']['id']
+    artifact_group = body_obj['data']['buildVariables']['group']
+    artifact_version = body_obj['data']['buildVariables']['version']
+    
+    response = client.search index: 'lead_time', body:
+      {
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "nested": {
+                  "path": "builds",
+                  "query": {
+                    "term": {
+                      "builds.artifact.id": {
+                        "value": artifact_id
+                      }
+                    }
+                  }
+                }
+              },
+              {
+                "nested": {
+                  "path": "builds",
+                  "query": {
+                    "term": {
+                      "builds.artifact.group": {
+                        "value": artifact_group
+                      }
+                    }
+                  }
+                }
+              },
+              {
+                "nested": {
+                  "path": "builds",
+                  "query": {
+                    "term": {
+                      "builds.artifact.version": {
+                        "value": artifact_version
+                      }
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    return response
+  end
+  
   def decode_body(headers, remote_address, body, default_codec, additional_codecs)
     content_type = headers.fetch("content_type", "")
     codec = additional_codecs.fetch(HttpUtil.getMimeType(content_type), default_codec)
-
+    
+    client = Elasticsearch::Client.new(:hosts => "#{@elastic_scheme}://#{@elastic_host}:#{@elastic_port}")
+    
     #decode the body to get the relevant info we want from JSON from jenkins
-    temp = JSON.parse(body)
-
+    body_obj = JSON.parse(body)
+    
     # Iterate through the jenkins' logs to access all of the fields we need
-    if temp['data']['buildVariables']['TYPE'] == "deploy"
-
-      artifact_id = temp['data']['buildVariables']['id']
-      artifact_group = temp['data']['buildVariables']['group']
-      artifact_version = temp['data']['buildVariables']['version']
-
-      client = Elasticsearch::Client.new(:hosts => "#{@elastic_scheme}://#{@elastic_host}:#{@elastic_port}")
-
+    if body_obj['data']['buildVariables']['TYPE'] == "deploy"
+      
       #client.search utilizes the elasticsearch ruby library to send a query to elasticsearch from the logstash plugin
-      repo_response = client.search index: 'lead_time', body:
-      {
-          "query": {
-              "bool": {
-                  "must": [
-                      {
-                          "nested": {
-                              "path": "builds",
-                              "query": {
-                                  "term": {
-                                      "builds.artifact.id": {
-                                          "value": artifact_id
-                                      }
-                                  }
-                              }
-                          }
-                      },
-                      {
-                          "nested": {
-                              "path": "builds",
-                              "query": {
-                                  "term": {
-                                      "builds.artifact.group": {
-                                          "value": artifact_group
-                                      }
-                                  }
-                              }
-                          }
-                      },
-                      {
-                          "nested": {
-                              "path": "builds",
-                              "query": {
-                                  "term": {
-                                      "builds.artifact.version": {
-                                          "value": artifact_version
-                                      }
-                                  }
-                              }
-                          }
-                      }
-                  ]
-              }
-          }
-      }
-
-      repo_response["hits"]["hits"].each do |hit|
+      lt_response = get_deploy_response(client, body_obj)
+      
+      lt_response["hits"]["hits"].each do |hit|
         lead_time_document = hit["_source"]
-
+        
         deploys = []
         #Determines if the lead_time doc build field exists
         if lead_time_document["deploys"].nil?
-          puts "running????"
           lead_time_document["deploys"] = deploys
-          end
-
-        start = lead_time_document["started_at"]
-        create = lead_time_document["created_at"]
-        finish = temp['@buildTimestamp']
-
-        start_date = DateTime.parse(start).to_time.to_i
-        finish_date = DateTime.parse(finish).to_time.to_i
-        create_date = DateTime.parse(create).to_time.to_i
+        end
+        
+        #Using the DateTime library, we retrieve the times from the lead-time documents, convert them to epoch format, then converted to an integer
+        start_date = DateTime.parse(lead_time_document["started_at"]).to_time.to_i
+        create_date = DateTime.parse(lead_time_document["created_at"]).to_time.to_i
+        finish_date = DateTime.parse(body_obj['@buildTimestamp']).to_time.to_i
+        
         total_time = finish_date - start_date
         progress_time = finish_date - create_date
-
+        
         lead_time_document["deploys"].push(
-           {
-             environment: temp['data']['buildVariables']['ENV'],
-             result: temp['data']['result'],
-             completed_at: temp['@buildTimestamp'],
-             total_time: total_time,
-             progress_time: progress_time,
-           }
+          {
+            environment: body_obj['data']['buildVariables']['ENV'],
+            result: body_obj['data']['result'],
+            completed_at: body_obj['@buildTimestamp'],
+            total_time: total_time,
+            progress_time: progress_time,
+          }
         )
-        if temp['data']['buildVariables']['ENV'] == "prod"
-          lead_time_document["prod"] =
+        
+        time_obj =
           {
-              total_time: total_time,
-              progress_time: progress_time
+            total_time: total_time,
+            progress_time: progress_time
           }
-        elsif temp['data']['buildVariables']['ENV'] == "qa"
-          lead_time_document["qa"] =
-          {
-              total_time: total_time,
-              progress_time: progress_time
-          }
-        elsif temp['data']['buildVariables']['ENV'] == "dev"
-          lead_time_document["dev"] =
-          {
-              total_time: total_time,
-              progress_time: progress_time
-          }
+        if body_obj['data']['buildVariables']['ENV'] == "prod"
+          lead_time_document["prod"] = time_obj
+        elsif body_obj['data']['buildVariables']['ENV'] == "qa"
+          lead_time_document["qa"] = time_obj
+        elsif body_obj['data']['buildVariables']['ENV'] == "dev"
+          lead_time_document["dev"] = time_obj
         else
           puts "other env"
         end
-
-        if temp['data']['result'] == 'SUCCESS'
-        lead = LogStash::Event.new(lead_time_document)
-        lead.set('[@metadata][index]', 'lead_time')
-        lead.set('[@metadata][id]', hit['_id'])
-        @queue << lead
+        
+        if body_obj['data']['result'] == 'SUCCESS'
+          lead = LogStash::Event.new(lead_time_document)
+          lead.set('[@metadata][index]', 'lead_time')
+          lead.set('[@metadata][id]', hit['_id'])
+          @queue << lead
         end
       end
-
-    elsif temp['data']['buildVariables']['TYPE'] == nil
-      if temp['data']['buildVariables']['POM_GROUPID'] != nil &&
-          temp['data']['buildVariables']['POM_ARTIFACTID'] != nil &&
-          temp['data']['buildVariables']['POM_VERSION'] != nil
-
-        commit_id = temp['data']['buildVariables']['GIT_COMMIT']
-        client = Elasticsearch::Client.new(:hosts => "#{@elastic_scheme}://#{@elastic_host}:#{@elastic_port}")
-
-        repo_response = client.search index: 'lead_time', body:
-        {
-          "query": {
-              "nested": {
-                  "path": "commits",
-                  "query": {
-                      "term": {
-                          "commits.id": {
-                              "value": commit_id
-                          }
-                      }
-                  }
-              }
-          }
-        }
-
+    
+    elsif body_obj['data']['buildVariables']['TYPE'] == nil
+      if body_obj['data']['buildVariables']['POM_GROUPID'] != nil &&
+        body_obj['data']['buildVariables']['POM_ARTIFACTID'] != nil &&
+        body_obj['data']['buildVariables']['POM_VERSION'] != nil
+        
+        lt_response = get_build_response(client, body_obj)
         builds = []
-        repo_response["hits"]["hits"].each do |hit|
+        
+        lt_response["hits"]["hits"].each do |hit|
           lead_time_document = hit["_source"]
-
-
+          
           #Determines if the lead_time doc build field exists
           if lead_time_document["builds"].nil?
             lead_time_document["builds"] = builds
           end
-
-          lead_time_document["builds"].push({artifact:
-          {
-            id: temp['data']['buildVariables']['POM_ARTIFACTID'],
-            group: temp['data']['buildVariables']['POM_GROUPID'],
-            name: temp['data']['buildVariables']['JOB_NAME'],
-            version: temp['data']['buildVariables']['POM_VERSION'],
-            packaging: temp['data']['buildVariables']['POM_PACKAGING']
-          },
-            result: temp['data']['result'],
-            built_at: temp['@buildTimestamp']
+          
+          lead_time_document["builds"].push(
+            {
+              artifact:
+                {
+                  id: body_obj['data']['buildVariables']['POM_ARTIFACTID'],
+                  group: body_obj['data']['buildVariables']['POM_GROUPID'],
+                  name: body_obj['data']['buildVariables']['JOB_NAME'],
+                  version: body_obj['data']['buildVariables']['POM_VERSION'],
+                  packaging: body_obj['data']['buildVariables']['POM_PACKAGING']
+                },
+              result: body_obj['data']['result'],
+              built_at: body_obj['@buildTimestamp']
             }
           )
-
-          if temp['data']['result'] == 'SUCCESS'
+          
+          if body_obj['data']['result'] == 'SUCCESS'
             lead = LogStash::Event.new(lead_time_document)
             lead.set('[@metadata][index]', 'lead_time')
             lead.set('[@metadata][id]', hit['_id'])
@@ -219,25 +220,25 @@ class LogStash::Inputs::Jenkins < LogStash::Inputs::Http
           end
         end
       else
-        puts "build that isn't maven build or deploy"
+        puts "Build job that isn't a maven job"
       end
     else
-      puts "JON IS NUB"
+      puts "Job is neither a build nor deploy"
     end
-
-    gg = LogStash::Event.new(temp)
-    gg.set('[@metadata][index]', 'build')
-    push_decoded_event(headers, remote_address, gg)
-
+    
+    #gg = LogStash::Event.new(body_obj)
+    #gg.set('[@metadata][index]', 'build')
+    #push_decoded_event(headers, remote_address, gg)
+    
     #codec.decode(body) { |event| push_decoded_event(headers, remote_address, event) }
     codec.flush { |event| push_decoded_event(headers, remote_address, event) }
     true
   rescue => e
     @logger.error(
-        "unable to process event.",
-        :message => e.message,
-        :class => e.class.name,
-        :backtrace => e.backtrace
+      "unable to process event.",
+      :message => e.message,
+      :class => e.class.name,
+      :backtrace => e.backtrace
     )
     false
   end
